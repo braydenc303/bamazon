6bc82e46bc41c2810786a1571683559bd271a61d
Brayden Copley <braydenc303@gmail.com>
2018-12-27 09:07:06 -0700

    Made the code less vulnerable to sql injection and created a better workflow.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -26,47 +26,49 @@ var connection = mysql.createConnection({
     readProducts();
   });
 // Then create a Node application called bamazonCustomer.js. Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
-var items = [];
-var prices = [];
-var qty = [];
 
-var currentStock = [];
 function readProducts() {
+    var items = [];
     console.log("Selecting all products...\n");
     connection.query("SELECT * FROM products", function(err, res) {
       if (err) throw err;
       // Log all results of the SELECT statement
       res.forEach(function(item){
           console.log(`Item Number: ${item.item_id} | Item Name: ${item.product_name} | Department: ${item.department_name} | Price: $ ${item.price}`);
-          //This would also be a good place to push item choices and qty into an array to check, or, I could simply save the response object for later reference.
           items.push(item.product_name);
-          prices.push(item.price);
-          qty.push(item.stock_qty);
       });
       currentStock = res;
-    //   console.log(currentStock);
-    //   console.log(items, qty);
-    //   connection.end();
-    order();
+      inquirer
+      .prompt([
+          {
+            name: "action",
+            type: "list",
+            message: "What would you like to do next?",
+            choices: ["Place an order.", "Exit the system."]
+          }
+      ]).then(function(answer){
+          if(answer.action === "Place an order."){
+            order(items);
+          } else {
+              console.log("Thanks. We will see you next time.");
+              connection.end();
+              return false;
+          }
+      })
+    
     });
   };
 
 // The app should then prompt users with two messages.
-function order(){
+function order(items){
   inquirer
     .prompt([
         // The first should ask them the ID of the product they would like to buy.
         {
             name: "item",
-            type: "input",
-            message: "\nEnter the item number you would like to order.",
-            validate: function(input){
-                if(typeof parseInt(input.item) === "number") {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
+            type: "list",
+            message: "\nChoose the item you would like to order.\n",
+            choices: items,
         },
         // The second message should ask how many units of the product they would like to buy.
         {
@@ -87,33 +89,28 @@ function order(){
             // If your store does have enough of the product, you should fulfill the customer's order.
             // This means updating the SQL database to reflect the remaining quantity.
             // Once the update goes through, show the customer the total cost of their purchase.
-            var index = inquirerRes.item - 1;
-            var quant = inquirerRes.quant;
-
-            // var item = items[inquirerRes.item];
-            // console.log(index);
-            if(inquirerRes.quant <= qty[index]){
-                console.log(inquirerRes);
-                connection.query(
-                    "UPDATE products SET ? WHERE ?",
-                    [
-                        {
-                            stock_qty: qty[index]-inquirerRes.quant
-                        },
-                        {
-                            item_id: inquirerRes.item
-                        }
-                    ]
-                );
-                console.log(`Thank you. your order of ${quant} ${items[index]} for a total of $${quant * prices[index]} will ship soon. We will notify you as soon as it is on it's way. Please print this page for your records.`)
-            } else {
-                // If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
-                console.log(`\nInsufficient quantity. We only have ${qty[index]} on hand. Please place a smaller order.\n`);
-                order();
-            }
-
+            connection.query(`SELECT * FROM products WHERE ?`,[{product_name: inquirerRes.item}], function(err, res){
+                if (err) throw err;
+                if (inquirerRes.quant <= res[0].stock_qty){
+                    connection.query(`UPDATE products SET ? WHERE ?`, 
+                    [{
+                        stock_qty: res[0].stock_qty - inquirerRes.quant
+                    },{
+                        product_name: inquirerRes.item
+                    }
+                    ], function(err, response){
+                        if (err) throw err;
+                        console.log(`Thank you. Your order of ${inquirerRes.quant} ${inquirerRes.item} for a total of $${inquirerRes.quant * res[0].price} will ship soon.\nWe will notify you as soon as it is on it's way.\nPlease print this page for your records.\n`);
+                        readProducts();
+                    });
+                } else {
+                    console.log(`We are sorry. We only have ${res[0].stock_qty} on hand. Please place a smaller order.`);
+                    readProducts();
+                }
+            })
         });
 };
+
 
 
 
